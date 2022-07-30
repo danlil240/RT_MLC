1f07a57b8394fae209214cd28057cf13b2841a4a
Daniel <danlil240@gamil.com>
2022-07-30 15:16:28 +0300

    'update'

--- plotComparison.m
+++ plotComparison.m
@@ -7,7 +7,7 @@ load('exp2sin.mat');
 Y_sin=Y_best;
 start=find(T>0.23,1);
 final=find(T>0.4,1);
-
+%% Plot Comparison
 
 figure('DefaultAxesFontName','David','DefaultAxesFontSize',12, 'DefaultAxesFontWeight', 'bold')
 
@@ -51,6 +51,48 @@ plot(T1(start:final)-0.23,Y_1(start:final,4),'linewidth',1.2)
 plot(T1(start:final)-0.23,Y_sin(start:final,4),'linewidth',1.2)
 ylabel('u[A]')
 legend({'LQR','MLC[gain,add]','MLC[gain,add,sin]'})
+%     xlim([0.23,0.4])
+hold off
+xlabel('Time[s]')
+% ,'FontSize', 12,'style','bold'
+
+%% Plot MLC
+
+
+figure('DefaultAxesFontName','David','DefaultAxesFontSize',12, 'DefaultAxesFontWeight', 'bold')
+
+subplot(4,1,1)
+plot(T1(start:final)-0.23,Y_1(start:final,1),'linewidth',1.2,'displayName','MLC[gain,add]')
+hold on
+legend
+ylabel('Displacment[\mum]');
+%     title(m);
+%     xlim([0.23,0.4])
+hold off
+
+subplot(4,1,2)
+plot(T1(start:final)-0.23,Y_1(start:final,2),'linewidth',1.2,'displayName','MLC[gain,add]')
+hold on
+legend
+ylabel('Velocity[\mum/s]');
+%     xlim([0.23,0.4])
+hold off
+
+subplot(4,1,3)
+plot(T1(start:final)-0.23,Y_1(start:final,3),'linewidth',1.2,'displayName','MLC[gain,add]')
+hold on
+%     str = '$$ \int_{0}^{t} J dt $$';
+%     ylabel(str,'Interpreter','latex')
+ylabel('J')
+legend
+%     xlim([0.23,0.4])
+hold off
+
+subplot(4,1,4)
+plot(T(start:final)-0.23,Y_1(start:final,4),'linewidth',1.2,'displayName','MLC[gain,add]')
+hold on
+ylabel('u[A]')
+legend
 %     xlim([0.23,0.4])
 hold off
 xlabel('Time[s]')
